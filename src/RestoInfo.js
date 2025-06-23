f463619f9499ad6c44a0894070665acea5b99a3f
codercoder61 <mohamedknoud@gmail.com>
2025-06-23 14:55:20 +0200

    Update RestoInfo.js

--- src/RestoInfo.js
+++ src/RestoInfo.js
@@ -227,7 +227,7 @@ useEffect(()=>{
                 quantity: 1,
                 mealPrice: meals[meals.findIndex(meal => meal.id === index)].price,
                 mealId: meals[meals.findIndex(meal => meal.id === index)].id,
-                restaurantId:meals[meals.findIndex(meal => meal.id === index)].id_restaurantOwner,
+                restaurantId:meals[meals.findIndex(meal => meal.id === index)].meal.id_restaurantOwner,
                 restaurantName:res.restaurantName
             });
         }

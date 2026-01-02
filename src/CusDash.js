2b5ff4e4d5bfe30f4726d5503ae9c74615f1d534
codercoder61 <mohamedknoud@gmail.com>
2026-01-03 00:16:37 +0100

    Update CusDash.js

--- src/CusDash.js
+++ src/CusDash.js
@@ -1280,7 +1280,7 @@ style={{ border: 'none', outline: 'none', height: '20px', width: '60px', }}
             </div>
 
             <div style={{marginTop:'20px'}}>
-{filteredMeals && <h1 style={{fontSize:'1.3em'}}>Most Ordered Meals 🤩</h1>}
+{filteredMeals && filteredMeals.length>0 && <h1 style={{fontSize:'1.3em'}}>Most Ordered Meals 🤩</h1>}
                  <div id="meals">
               {filteredMeals && filteredMeals.map((elm) => (
   <div key={elm.meal.id} className="meal">
